858e857a3653072b8c8c1f8656889e180d92d7d2
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-26 20:07:50 -0300

    resolver a soma de dois números

--- 03_soma_de_dois_numeros.py
+++ 03_soma_de_dois_numeros.py
@@ -1,1 +1,8 @@
 '''Desenvolva um sistema que solicite, ao usuário, dois números, some os dois e exiba o resultado'''
+
+numero1=float(input("Digite um número: "))
+numero2=float(input("Digite um número: "))
+
+soma = numero1+numero2 
+
+print(f"A soma entre {numero1} e {numero2} é: {soma}")
