e906feef03b627ceab8093ca086e318ed3d94fc9
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-26 20:14:03 -0300

    Resolução calculadora de idade

--- 04_calculadora_de_idade.py
+++ 04_calculadora_de_idade.py
@@ -1,1 +1,9 @@
 '''Desenvolva um sistema que solicite o ano de nascimento do usuário, calcule e exiba a sua idade atual'''
+
+ano_atual = 2026
+
+ano_nascimento = int(input("Digite o seu ano de nascimento: "))
+
+idade = ano_atual - ano_nascimento
+
+print(f"Em {ano_atual}, você tem (ou completará {idade} anos.")
