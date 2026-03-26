d4eebb1e81fdd598583dc0d1a625c0e1bb0cf0e0
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-26 20:39:06 -0300

    Resolve calculadora de media

--- 05_calculadora_de_media.py
+++ 05_calculadora_de_media.py
@@ -1,1 +1,13 @@
 '''Desenvolva um sistema que solicite três números inteiros, calcule e exiba a média dentre estes números.'''
+
+# Solicita os três números inteiros ao usuário
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+n3 = int(input("Digite o terceiro número: "))
+
+# Realiza o cálculo da média
+# A soma deve estar entre parênteses para ser feita antes da divisão
+media = (n1 + n2 + n3) / 3
+
+# Exibe o resultado final
+print(f"A média dos números {n1}, {n2} e {n3} é: {media}")
