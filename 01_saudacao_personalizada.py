4780bf80345570dbb8e59ff4d1ded333cc4a40ac
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-26 21:02:53 -0300

    Resolve saudação personalizada

--- 01_saudacao_personalizada.py
+++ 01_saudacao_personalizada.py
@@ -1,3 +1,5 @@
 '''Crie um programa que mostre uma mensagem de boas vindas personalizada.
 “Olá mundo, aqui vamos nós iniciar no universo da programação!”'''
+
+# Exibe a mensagem de boas-vindas personalizada
 print ("Olá mundo, aqui vamos nós iniciar no universo da programação")
