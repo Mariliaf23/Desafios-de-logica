350da6d7d3c682646b213fe706710fe9287ca25c
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-26 21:02:34 -0300

    Resolve saudação nominal

--- 02_saudacao_nominal.py
+++ 02_saudacao_nominal.py
@@ -1,5 +1,7 @@
 '''Crie um programa que recebe o nome do usuário e mostre uma mensagem de boas vindas personalizada com o nome do mesmo.
 “Olá {usuário}, seja bem vindo. Aqui vamos iniciar no universo da programação!”'''
-
+# Recebe o nome do usuário
 usuário = input("Digite um nome: ")
+
+# Exibe a mensagem de boas-vindas personalizada
 print(f"Olá {usuário}, seja bem vindo. Aqui vamos iniciar no universo da programação!")
