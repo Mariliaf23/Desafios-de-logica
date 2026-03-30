e2b419a5f81b7f50828bd83cc83149648201b29b
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 19:13:31 -0300

    Resolve o problema número PAR ou ÍMPAR

--- 21_aluno_favorito.py
+++ 21_aluno_favorito.py
@@ -2,3 +2,21 @@
 Objetivo: Desenvolver um programa que receba um número e informe se ele é par ou ímpar.
 Regra: Um número é par quando é divisível por 2. Caso contrário, ele é ímpar. O sistema deve continuar a realizar verificações de par ou ímpar até que o usuário interrompa sua execução.
 '''
+
+
+while True:
+    # Entrada do número
+    num_str = input("\nDigite um número (ou 'sair' para encerrar): ")
+
+    # Condição de parada
+    if num_str.lower() == 'sair':
+        print("Disputa encerrada!")
+        break
+
+    # Conversão e verificação
+    numero = int(num_str)
+
+    if numero % 2 == 0:
+        print(f"O número {numero} é PAR!")
+    else:
+        print(f"O número {numero} é ÍMPAR!")
