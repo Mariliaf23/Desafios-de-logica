7709351fa5ae8636210fb8309a64559376b32244
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 19:15:34 -0300

    Resolve paixão de Gabriel

--- 26_o_ultimo_romantico.py
+++ 26_o_ultimo_romantico.py
@@ -2,3 +2,10 @@
 Objetivo: Desenvolva um sistema que contabilize por quantas garotas ele se apaixonou enquanto estudava programação. O sistema deve mostrar na tela uma contagem progressiva de 1 a 10.
 Regras: A contagem deve ser exibida número por número, em ordem crescente, iniciando de 1 e indo até 10.
 '''
+
+
+# Contagem progressiva das paixões do Gabriel
+print("Contagem de paixões durante os estudos de programação:")
+
+for i in range(1, 11):
+    print(f"{i}ª garota")
