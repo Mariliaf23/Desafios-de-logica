37999fdb52bdf932340b82bc096d44a808482c7a
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 19:36:02 -0300

    Resolve sorteio - adivinhe o número

--- 37_adivinha_o_numero.py
+++ 37_adivinha_o_numero.py
@@ -2,3 +2,29 @@
 Regra: use a biblioteca útil para sortear o número.
 Complicando: Faça o sistema informar se o número que o usuário informou é maior ou menor que o número sorteado. O sistema deve indicar quantas tentativas foram necessárias para acertar o número.
 '''
+
+
+import random
+
+# Sorteio do número entre 1 e 10
+numero_sorteado = random.randint(1, 10)
+tentativas = 0
+acertou = False
+
+print("--- Jogo da Adivinhação ---")
+print("Tente adivinhar o número que eu sorteei entre 1 e 10!")
+
+while not acertou:
+    palpite = int(input("\nQual o seu palpite? "))
+    tentativas += 1
+    
+    if palpite == numero_sorteado:
+        print(f"Parabéns! Você acertou o número {numero_sorteado}.")
+        print(f"Total de tentativas: {tentativas}")
+        acertou = True
+    elif palpite < numero_sorteado:
+        print("Errou! O número sorteado é MAIOR que o seu palpite.")
+    else:
+        print("Errou! O número sorteado é MENOR que o seu palpite.")
+
+print("Fim de jogo!")
