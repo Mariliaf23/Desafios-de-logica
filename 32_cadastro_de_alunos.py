5ed86a1ea5b21fb59c541d448b33e0a4a8cb85aa
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 19:33:23 -0300

    Resolve sistema de mensalidade pagas

--- 32_cadastro_de_alunos.py
+++ 32_cadastro_de_alunos.py
@@ -2,3 +2,25 @@
 Objetivo: Desenvolver um sistema que receba os valores das mensalidades pagas pelos alunos, até que o usuário informe que os pagamentos acabaram. Ao final, o sistema deve exibir o total pago em mensalidades.
 Regras: O sistema deve permitir a entrada de múltiplos valores (representando mensalidades pagas), um por vez. A entrada de valores deve continuar até o usuário indicar que não há mais pagamentos. Ao final, o programa deve somar todos os valores informados e exibir o total pago.
 '''
+
+
+total_pago = 0.0
+
+print("--- Sistema de Mensalidades Senac ---")
+print("(Digite '0' para encerrar e ver o total)")
+
+while True:
+    # Recebe o valor da mensalidade
+    entrada = input("Valor da mensalidade paga: R$ ").replace(",", ".")
+    valor = float(entrada)
+
+    # Condição de parada (se o valor for 0, encerra o loop)
+    if valor == 0:
+        break
+    
+    # Soma o valor ao total acumulado
+    total_pago += valor
+
+print("-" * 35)
+print(f"Total arrecadado em mensalidades: R$ {total_pago:.2f}")
+print("Sistema finalizado.")
