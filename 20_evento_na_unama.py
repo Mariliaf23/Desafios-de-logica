455d937727637ec8e24397422283c0455307e9aa
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 19:12:51 -0300

    Resolve sistema para identificar maior idade

--- 20_evento_na_unama.py
+++ 20_evento_na_unama.py
@@ -2,3 +2,13 @@
 Objetivo: Desenvolver um programa que receba a idade de um participante e informe se ele é maior de idade.
 Regra: Considera-se maior de idade quem tem 18 anos ou mais.
 '''
+
+
+# Entrada da idade
+idade = int(input("Informe a idade do participante: "))
+
+# Verificação da maioridade
+if idade >= 18:
+    print("O participante é maior de idade. Acesso permitido ao evento!")
+else:
+    print("O participante é menor de idade. Acesso restrito.")
