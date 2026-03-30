d3faa21a0a4117024a220034bd269c7d8fee40f7
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 19:15:47 -0300

    Resolve coleta da coxinha

--- 27_coleta_da_coxinha.py
+++ 27_coleta_da_coxinha.py
@@ -2,3 +2,18 @@
 Objetivo: Desenvolver um sistema que receba o valor coletado por 5 alunos e calcule a soma total da arrecadação. O sistema deve exibir o valor total coletado.
 Regras: O programa deve receber 5 valores numéricos, representando o valor contribuído por cada aluno. Após somar os valores: Se o total for maior que R$ 40,00, o sistema deve exibir o valor total e a mensagem: "Emanuel sumiu!“. Caso contrário, o sistema deve mostrar apenas o valor total da coleta.
 '''
+
+
+total_coleta = 0
+
+# Recebendo o valor de 5 alunos
+for i in range(1, 6):
+    valor = float(input(f"Valor do {i}º aluno: R$ "))
+    total_coleta += valor
+
+print("-" * 30)
+print(f"Total arrecadado: R$ {total_coleta:.2f}")
+
+# Regra do Emanuel
+if total_coleta > 40.00:
+    print("Emanuel sumiu!")
